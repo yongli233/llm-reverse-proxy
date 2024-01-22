172cd367ccbe35580fc193aece5443764d5d21ba
yongman <135026666+yongli233@users.noreply.github.com>
2024-01-23 02:36:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const chatLimiter = rateLimit({
   },
 });
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(helmet());
 app.use(cors());
 app.set('trust proxy', 1);
